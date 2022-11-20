dda7e72ac0b08042ab0582b2093afa3007246f98
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2022-11-20 10:52:45 +0100

    feat: employee to people refactoring

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EmployeeService } from '../../services/employee.service';
 import { EmployeeModel } from '../../model/employee.model';
@@ -12,7 +11,7 @@ import { EmployeeModel } from '../../model/employee.model';
   styleUrls: ['employee-list.component.css']
 })
 export class EmployeeListComponent {
-  constructor(private _httpClient: HttpClient, private _employeeService: EmployeeService) { }
+  constructor(private _employeeService: EmployeeService) { }
   data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
 
 }
