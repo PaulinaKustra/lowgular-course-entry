729c872e286e1311c87b666e54d199efdf02b2a5
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2022-11-22 12:24:26 +0100

    feat: PersonModel created, components upgraded accordingly

--- src/app/ui/employees-faces/employees-faces.component.ts
+++ src/app/ui/employees-faces/employees-faces.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import {Observable} from "rxjs";
-import {EmployeeModel} from "../../model/employee.model";
+import { PersonModel} from '../../model/person.model';
 import {EmployeeService} from "../../services/employee.service";
 
 @Component({
@@ -11,5 +11,5 @@ import {EmployeeService} from "../../services/employee.service";
 })
 export class EmployeesFacesComponent {
   constructor(private _employeeService: EmployeeService) { }
-  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
+  data$: Observable<PersonModel[] | null> = this._employeeService.getAll();
 }
