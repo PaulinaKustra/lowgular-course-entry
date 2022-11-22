e731fcb9b2f5eafbdc2611d405a95c25842fbe74
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2022-11-22 16:59:09 +0100

    feat: form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {EmployeesFacesComponentModule} from "./ui/employees-faces/employees-face
 import {EmployeeServiceModule} from "./services/employee.service-module";
 import {ProjectServiceModule} from "./services/project.service-module";
 import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
+import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,7 +22,8 @@ import {ProjectListComponentModule} from "./ui/project-list/project-list.compone
     EmployeesFacesComponentModule,
     EmployeeServiceModule,
     ProjectServiceModule,
-    ProjectListComponentModule
+    ProjectListComponentModule,
+    EmployeeFormComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent],
