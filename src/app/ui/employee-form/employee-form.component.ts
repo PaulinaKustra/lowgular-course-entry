cf94663b73a29c735a17f00bdc77d2afeda50c7c
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2022-11-23 14:49:49 +0100

    feat: onButtonClicked alert added

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -10,7 +10,13 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
     name: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required]),
     salary: new FormControl(null, [Validators.min(0), Validators.required]),
     age: new FormControl(null, [Validators.min(0), Validators.required]),
   });
+
+  onButtonClicked(text: string, form: {email: string, age: number, salary: number}  ) {
+    alert(`User was successfully added to the database. Email:  ${this.employeeForm.value.email}, Age: ${this.employeeForm.value.age}, Salary: ${this.employeeForm.value.salary}`);
+  }
+
 }
